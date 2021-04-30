34793811b275c20ba54daa138b3025ab58193894
MASARU OHASHI JUNIOR <masaru.ohashi@gmail.com>
2021-04-29 22:46:11 -0300

    JS completo

--- Queue.js
+++ Queue.js
@@ -21,11 +21,11 @@ ohashiQueue = () => {
 }
 
 const marvelMovies = ohashiQueue()
-console.log('FIFO--------')
+console.log('Inserindo os elementos na Queue')
 console.log('INPUT: '+marvelMovies.add('Iron Man'))
 console.log('INPUT: '+marvelMovies.add('Captain America'))
 console.log('INPUT: '+marvelMovies.add('Incredible Hulk'))
-console.log('Removendo os elementos')
+console.log('Removendo os elementos da Queue')
 console.log('OUTPUT: '+marvelMovies.remove())
 console.log('OUTPUT: '+marvelMovies.remove())
 console.log('OUTPUT: '+marvelMovies.remove())
