301a9ebc227b7dbdfb3fe4eb7deca2c6dd1b141b
MASARU OHASHI JUNIOR <masaru.ohashi@gmail.com>
2021-04-29 22:38:58 -0300

    primeiro commit

---
+++ Queue.js
@@ -0,0 +1,31 @@
+ohashiQueue = () => {
+    const queue = []
+    return {
+        add(x) {
+            queue.unshift(x)
+            return x
+        },
+        remove(){
+            return queue.pop()            
+        },
+        next(){
+            return queue[queue.length-1]
+        },
+        length(){
+            return queue.length
+        },
+        empty(){
+            return queue.length === 0
+        }
+    }
+}
+
+const marvelMovies = ohashiQueue()
+console.log('FIFO--------')
+console.log('INPUT: '+marvelMovies.add('Iron Man'))
+console.log('INPUT: '+marvelMovies.add('Captain America'))
+console.log('INPUT: '+marvelMovies.add('Incredible Hulk'))
+console.log('Removendo os elementos')
+console.log('OUTPUT: '+marvelMovies.remove())
+console.log('OUTPUT: '+marvelMovies.remove())
+console.log('OUTPUT: '+marvelMovies.remove())
---
+++ Queue.py
@@ -0,0 +1,14 @@
+queue = []
+
+queue.append('a')
+queue.append('b')
+queue.append('c')
+
+print("Fila Inicial")
+print(queue)
+print("Elementos retirados da fila")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print("\nFila depois dos elementos terem sido removidos.")
+print(queue)
